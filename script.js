634e7a2376159be83421592f61fcf3846c579316
Matthias Schmitze <m6schmitz123@gmail.comm>
2023-02-28 10:57:18 +0100

    basic build

---
+++ script.js
@@ -0,0 +1,17 @@
+let fields=[];
+
+let currentShape='cross';
+
+function fillShape(id){
+    if (currentShape =='cross') {
+        currentShape='circle';
+        
+    }
+    else{
+        
+    }
+    fields[id]=currentShape;
+
+    
+
+}
